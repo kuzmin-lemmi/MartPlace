d539f646d2ee05c581c1505169565110c43e311d
kuzmin-lemmi <kuzmin.lemmi@gmail.com>
2020-02-24 23:37:28 +0400

    added featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(function () {
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "15px",
+    });
+
+    $('.featured__slider').slick({
+        nextArrow:'<button class="slick-arrow slick-next"><img src="images/featured/chevron-right.png" alt=""></img></button>',
+        prevArrow:' <button class="slick-arrow slick-prev"><img src="images/featured/chevron-left.png" alt=""></img></button>',
+        slidesToShow:1,
+        slidesToScroll:1
+    });
 
 });
